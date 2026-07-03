a8435a85013266af873dbcd9626c5e062aa8de7b
dharshan08092005 <dharshanpersonal01@gmail.com>
2026-07-03 12:24:15 +0530

    Hello Card

---
+++ Level1/name&age.py
@@ -0,0 +1,5 @@
+# input(), variables, casting, f-strings, print()
+user_name = input("Enter your name:")
+user_age = int(input("Enter your age:"))
+
+print(f"Hi {user_name} welcome. you are {user_age} years old!")
