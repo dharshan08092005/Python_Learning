fe59026bafe7eef2e0273c2d395ab79c4315f1fe
dharshan08092005 <dharshanpersonal01@gmail.com>
2026-07-03 12:31:38 +0530

    data type explorer

---
+++ Level1/dataType.py
@@ -0,0 +1,12 @@
+name  = "SD"
+age = 20
+height = 171.5
+present = True
+absent = None
+
+print(f"""Today's record:
+      {type(name)}{name} of age {type(age)}{age} years and height {type(height)}{height}cm is present: {type(present)}{present} or absent:{type(absent)}{absent} today.
+       """)
+
+#None value cannot be printed
+ 
